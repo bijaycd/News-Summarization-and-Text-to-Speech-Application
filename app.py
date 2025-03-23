2984b6808128e76d928518f3736f76cf68809e15
Bijay Chandra Das <bdas64316@gmail.com>
2025-03-23 14:40:37 +0530

    fix

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-FASTAPI_URL = "https://bijayjr-SentimentNews.hf.space"
+FASTAPI_URL = "http://127.0.0.1:8000"
 
 # Use normal width layout
 st.set_page_config(page_title="Insightful News AI", page_icon = "🤖", layout="centered")
